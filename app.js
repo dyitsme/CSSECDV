c01fb81869804f8086a7254dce18de73b4b623aa
Denzel <denzel_bryan_yongco@dlsu.edu.ph>
2024-01-18 15:33:51 +0800

    Added semicolon

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const routes = require("./routes/routes")
+const routes = require("./routes/routes");
 const app = express();
 
 app.set("view engine", "ejs");
