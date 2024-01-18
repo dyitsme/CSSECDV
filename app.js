142b6ecb62cc84f258c4fa30ef2265b6e2b30173
Denzel <denzel_bryan_yongco@dlsu.edu.ph>
2024-01-18 15:32:20 +0800

    Added view engine and routes

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-
+const routes = require("./routes/routes")
 const app = express();
+
+app.set("view engine", "ejs");
 
 app.listen(process.env.PORT, () => {
   console.log("Server started on port "+ process.env.PORT);
 });
 
+app.use("/", routes)
---
+++ controllers/loginController.js
@@ -0,0 +1,12 @@
+const loginView = (req, res) => {
+  res.render("login");
+}
+
+const registerView = (req, res) => {
+  res.render("register")
+}
+
+module.exports = {
+  registerView,
+  loginView
+}
